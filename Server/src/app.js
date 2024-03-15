773d2c5fb074df4fc9e0599abe8b76f9a1126c9d
fenixreds <jue16571@gmail.com>
2024-03-15 12:34:14 -0500

    Test incluido

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const router = require('./routes');
 const server=express();
 
 server.use((req, res, next) => {
--- Server/test/index.test.js
+++ Server/test/index.test.js
@@ -1,2 +1,107 @@
+const app = require('../src/app');
+const session = require('supertest');//para generar server ficticio
+const agent = session(app);//para levantar server fake
 
+
+describe('Test de RUTAS', ()=>{
+
+    describe('GET /rickandmorty/character/:id', ()=>{
+        it('Responde con status: 200', async ()=>{
+            const {body}=await agent.get('/rickandmorty/character/1').trustLocalhost();
+
+            console.log(body);
+            await agent.get('/rickandmorty/character/1').expect(200);
+        });
+
+        it('Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image"', async ()=>{
+            let { body } = await agent.get('/rickandmorty/character/1');
+            expect(body).toHaveProperty('id'&&'name'&&'gender'&&'status'&&'origin'&&'image');
+        });
+
+        it('Si hay un error responde con status: 500', async ()=>{
+            await agent.get('/rickandmorty/character/25639').expect(500);
+        });
+    });
+
+    describe('GET /rickandmorty/login', ()=>{
+        it('Con el login correcto debe retornar access:true', async ()=>{
+            let { body } = await agent.get('/rickandmorty/login?email=batman@gmail.com&password=robin1234');
+            expect(body.access).toBeTruthy();
+        });
+        it('Con el login incorrecto debe retornar access:false', async ()=>{
+            let { body } = await agent.get('/rickandmorty/login?email=batman@gmail.com&password=robin1243');
+            expect(body.access).toBeFalsy();
+        });
+    });
+
+    describe('POST /rickandmorty/fav', ()=>{
+        //creo personaje ficticio
+        let fakeBody = {
+            id:25,
+            name:"Batman Sanchez",
+            gender:"Male",
+            species:"BAT",
+            origin: "SAD",
+            image:"SOME URL",
+            status:"ALIVE"
+        }
+        let fakeBody2 = {
+            id:27,
+            name:"Batman Lopez",
+            gender:"Male",
+            species:"BAT",
+            origin: "SAD",
+            image:"SOME URL",
+            status:"ALIVE"
+        }
+        it('Debe retornar el body como arreglo', async ()=>{
+            const { body } = await agent.post('/rickandmorty/fav').send(fakeBody);
+            expect(body[0]).toEqual(fakeBody);
+        });
+        it('Debe retornar los personajes previos', async ()=>{
+            const { body } = await agent.post('/rickandmorty/fav').send(fakeBody2);
+            expect(body).toEqual([fakeBody, fakeBody2]);
+        });
+    });
+
+    describe('DELETE /rickandmorty/fav/:id', ()=>{
+        //creo personaje ficticio
+        let fakeBody = {
+            id:25,
+            name:"Batman Sanchez",
+            gender:"Male",
+            species:"BAT",
+            origin: "SAD",
+            image:"SOME URL",
+            status:"ALIVE"
+        }
+        let fakeBody2 = {
+            id:27,
+            name:"Batman Lopez",
+            gender:"Male",
+            species:"BAT",
+            origin: "SAD",
+            image:"SOME URL",
+            status:"ALIVE"
+        }
+        let fakeBody3 = {
+            id:35,
+            name:"Batmana Lopez",
+            gender:"Female",
+            species:"BAT",
+            origin: "SAD",
+            image:"SOME URL",
+            status:"ALIVE"
+        }
+        it('Returna un arreglo con los 2 chars previos', async ()=>{
+            const { body } = await agent.delete('/rickandmorty/fav/256');
+            expect(body.length).toEqual(2);
+        });
+        it('Debe retornar los personajes previos', async ()=>{
+            await agent.post('/rickandmorty/fav').send(fakeBody3);
+            const { body } = await agent.delete('/rickandmorty/fav/35');
+            expect(body.length).toEqual(2);
+        });
+    });
+});
 
